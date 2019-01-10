a2ecf389150c3881b1542cb09ea9db2c19ed5e10
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2019-01-10 23:40:55 +0500

    1

---
+++ server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var geoip = require('geoip-lite');
+
+var ip = "207.97.227.239";
+var geo = geoip.lookup(ip);
+
+console.log(geo);
+
+//create a server object:
+http.createServer(function (req, res) {
+    console.log(req.connection.remoteAddress);
+    res.write('Hello World!'); //write a response
+    res.end(); //end the response
+}).listen(3000, function () {
+    console.log("server start at port 3000"); //the server object listens on port 3000
+});
