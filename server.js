f64ad03cb63584e7afb34c24f589a8e769fa51c7
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2019-01-10 23:50:16 +0500

    2

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ http.createServer(function (req, res) {
     console.log(req.connection.remoteAddress);
     res.write('Hello World!'); //write a response
     res.end(); //end the response
-}).listen(3000, function () {
+}).listen(80, function () {
     console.log("server start at port 3000"); //the server object listens on port 3000
 });
